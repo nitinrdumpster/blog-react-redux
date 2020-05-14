767c2b79caf349e9c467e0e98b5bb2fabb1b1791
Nitin Ranganath <nitinranganath@gmail.com>
2020-05-14 19:29:43 +0530

    Setup redux thunk for async requests

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+// Use redux thunk to make async request
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
+  dispatch({ type: "FETCH_POSTS", payload: response });
+};
