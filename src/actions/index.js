68bed220f65820a90578f08668ee715b7f0d8afc
Nitin Ranganath <nitinranganath@gmail.com>
2020-05-15 18:23:27 +0530

    Revert memoization. Implement alternate solution for overfetching.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,37 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 import _ from "lodash";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+
+  // Alternate method using Lodash chain
+  // _.chain(getState.posts)
+  //   .map("userId")
+  //   .uniq()
+  //   .forEach((id) => dispatch(fetchUser(id)))
+  //   .value();
+};
+
 // Use redux thunk to make async request
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-// To memoize a function, create a private function (_fetchUser in this case).
-// Export the private memoized function. 
-// Private function will take dispatch as an argument.
-export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
+
+// To memoize a function, create a private function (_fetchUser in this case).
+// Export the private memoized function.
+// Private function will take dispatch as an argument.
+
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
