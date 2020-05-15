16e786a56d02c40d0fc9fbde812ede2114bdc4fd
Nitin Ranganath <nitinranganath@gmail.com>
2020-05-15 17:56:48 +0530

    Fix overfetching by memoization

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
 
 // Use redux thunk to make async request
 export const fetchPosts = () => async (dispatch) => {
@@ -6,7 +7,11 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+// To memoize a function, create a private function (_fetchUser in this case).
+// Export the private memoized function. 
+// Private function will take dispatch as an argument.
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
